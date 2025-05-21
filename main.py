129887ed89d8ecd07d78eb90be71a705055cdbfb
Shoaib1M <118591167+Shoaib1M@users.noreply.github.com>
2025-05-21 16:07:40 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import smtplib
+import datetime as dt
+import random
+
+now=dt.datetime.now()
+weekday=now.weekday()
+
+if weekday==0:
+    with open("quotes.txt") as quote_file:
+        all_quotes=quote_file.readlines()
+        random_choice=random.choice(all_quotes)
+    my_email = "shoaibmunavary@gmail.com"
+    pswd = "rxlvoarxuhrdcyue"  # App password, not your main password
+
+    connection = smtplib.SMTP('smtp.gmail.com', 587, timeout=10)
+    connection.starttls()
+    connection.login(user=my_email, password=pswd)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=my_email,
+        msg=f"Subject:Monday Motivation\n\n{random_choice}"
+    )
+    connection.close()
+
+
+
+
+
